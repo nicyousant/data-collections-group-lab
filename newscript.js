0880241f155e46978aecd77eed337eeb88a2d388
nicyousant <90989304+nicyousant@users.noreply.github.com>
2025-09-07 22:51:01 -0400

    updated script.js

--- newscript.js
+++ newscript.js
@@ -1,69 +1,3 @@
-let index = 0; 
-let cellOne = "";
-let cellTwo = "";
-let cellThree = "";
-let cellFour = "";
-let currCell = "";
-
-let csvData = "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26";
-
-while (index < csvData.length) {
-  if (csvData[index] == ",") {
-    index++; //don't include the comma
-    if (cellOne.length == 0) {
-      cellOne = currCell; 
-    } else if (cellTwo.length == 0) {
-      cellTwo = currCell;
-    } else if (cellThree.length == 0) {
-      cellThree = currCell;
-    } 
-    currCell = ""; //reset current cell string
-  }
-  else if(csvData[index] == "\n"){
-    cellFour = currCell; //this is the last cell: print and reset everything to continue
-    console.log(`${cellOne},${cellTwo},${cellThree},${cellFour}`);
-    currCell = "";
-    cellOne = "";
-    cellTwo = "";
-    cellThree = "";
-    cellFour = "";
-    index++;
-  } 
-  else {
-    currCell += csvData[index]; 
-    index++;
-  }
-}
-
-
-
-// let rowData = csvData.split('\n')
-// let arrOfArr = []
-
-
-// for (i = 0 ; i < rowData.length ; i++) {
-//     let colData = rowData[i].split(',')
-//     arrOfArr.push(colData)
-// }
-
-// console.log(arrOfArr)
-
-// let arrObj = {}
-
-
-// for (j = 0 ; j < arrOfArr.length -1 ; j++ ) {
-    
-//     for ( k = 0 ; k < arrOfArr[j].length ; k++) {
-//         let newKey = arrOfArr[0][k];
-//         arrObj[newKey] = arrOfArr[j+1][k]
-//        console.log(newKey)
-//     }
-//     arrOfArr[j] = arrObj
-// }
-// console.log(arrOfArr)
-
-
-
 // Part 1. Refactoring Old Code
 console.log('Part 1: Refactoring Old Code')
 
@@ -76,13 +10,16 @@ function parseCSV(data) {
     return result;
 }
 
-//let csvData = `ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26`;
+let csvData = `ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26`;
 const parsedData = parseCSV(csvData);
 console.log(parsedData);
 
 
 
 
+
+
+//let csvData = "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26";
 
 // Part 2: Expanding Functionality
 console.log('Part 2: Expanding Functionality')
--- script2.js
+++ script2.js
@@ -1,17 +1,32 @@
-const csvData = `ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26`
+// const csvData = `ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26`
 
-let arrObj = [];
-let lines = csvData.split('\n');
-let headers = lines[0].split(',');
+// let arrObj = [];
+// let lines = csvData.split('\n');
+// let headers = lines[0].split(',');
 
-for (let i = 1; i < lines.length; i++) {
+// for (let i = 1; i < lines.length; i++) {
 
-    let rowData = lines[i].split('\n');
-    arrObj[i] = {};
+//     let rowData = lines[i].split('\n');
+//     arrObj[i] = {};
 
-    for (let j = 0; j < rowData.length; j++) {
-        console.log(headers[j]);
+//     for (let j = 0; j < rowData.length; j++) {
+//         console.log(headers[j]);
 
-        console.log(rowData[j])
-    }
+//         console.log(rowData[j])
+//     }
+// }
+
+
+// https://www.delftstack.com/howto/javascript/parse-csv-in-javascript/
+// https://stackoverflow.com/questions/48015270/steps-to-create-javascript-objects-from-text-data-out-of-a-csv-file-using-no-lib
+
+function parseCSV(data) {
+    const rows = data.split('\n');
+    const result = rows.map(row => row.split(','));
+    return result;
 }
+
+const csvData = `ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26`;
+const parsedData = parseCSV(csvData);
+console.log(parsedData);
+
